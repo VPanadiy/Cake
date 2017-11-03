98bb5940003cd1c8a4aaa4bf30328263f6475558
Splayd <VPanadiy@gmail.com>
2017-11-03 14:01:59 +0200

    adding header page

--- src/main/java/com/Aleksandr/Cake/controller/ProductController.java
+++ src/main/java/com/Aleksandr/Cake/controller/ProductController.java
@@ -1,12 +1,17 @@
 package com.Aleksandr.Cake.controller;
 
+import com.Aleksandr.Cake.model.User;
 import com.Aleksandr.Cake.repository.ProductRepository;
+import com.Aleksandr.Cake.serviceSecurity.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import static com.Aleksandr.utils.CONST.URL_ADD_PRODUCTS;
 import static com.Aleksandr.utils.CONST.URL_PRODUCTS;
@@ -15,6 +20,7 @@ import static com.Aleksandr.utils.ViewURLs.PRODUCTS_VIEW;
 @Controller
 public class ProductController {
 
+    private UserService userService;
     private ProductRepository productRepository;
 
     private final Logger logger = LoggerFactory.getLogger(ReadingListController.class);
@@ -22,6 +28,22 @@ public class ProductController {
     @Autowired
     public void setProductRepository(ProductRepository productRepository) {
         this.productRepository = productRepository;
+    }
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/include/header")
+    @ResponseBody
+    public String header(Model model) {
+        logger.info("Open the page header.html -- my logger");
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByEmail(auth.getName());
+        model.addAttribute("userName",
+                "Welcome " + user.getName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
+        return PRODUCTS_VIEW;
     }
 
     @RequestMapping(URL_PRODUCTS)
