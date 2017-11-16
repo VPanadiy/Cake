817caf52370ac5c89df10979c356815cb6023a43
VPanadiy <VPanadiy@gmail.com>
2017-11-16 21:50:59 +0200

    adding Spring WebFlow

--- src/main/java/com/Aleksandr/Cake/WebConfig.java
+++ src/main/java/com/Aleksandr/Cake/WebConfig.java
@@ -1,22 +1,38 @@
 package com.Aleksandr.Cake;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.webflow.mvc.servlet.FlowHandlerAdapter;
+import org.springframework.webflow.mvc.servlet.FlowHandlerMapping;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.StandardTemplateModeHandlers;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
 
 @Configuration
 @ComponentScan("com.Aleksandr.Cake.*")
 public class WebConfig extends WebMvcConfigurerAdapter {
+
     private final Logger LOGGER = LoggerFactory.getLogger(WebConfig.class);
+
+    @Autowired
+    private WebFlowConfig webFlowConfig;
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -37,14 +53,14 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
-    
+
     @Bean
     public CookieLocaleResolver localeResolver() {
         LOGGER.info("-- Initialization localeResolver in WebConfig!!!");
-        CookieLocaleResolver resolver= new CookieLocaleResolver();
+        CookieLocaleResolver resolver = new CookieLocaleResolver();
         resolver.setCookieDomain("myAppLocaleCookie");
         // 60 minutes
-        resolver.setCookieMaxAge(60*60);
+        resolver.setCookieMaxAge(60 * 60);
         return resolver;
     }
 
@@ -62,13 +78,35 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         registry.addInterceptor(localeChangeInterceptor());
     }
 
-//    @Bean(name = "viewResolver")
-//    public InternalResourceViewResolver getViewResolver() {
-//        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-//        viewResolver.setPrefix("/");
-//        viewResolver.setSuffix(".html");
-//        return viewResolver;
-//    }
+    @Bean
+    public TemplateResolver templateResolver() {
+        TemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(StandardTemplateModeHandlers.LEGACYHTML5.getTemplateModeName());
+        templateResolver.setCacheable(false);
+        templateResolver.setCharacterEncoding("UTF-8");
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine springTemplateEngine = new SpringTemplateEngine();
+        springTemplateEngine.setTemplateResolver(templateResolver());
+        springTemplateEngine.addDialect(new LayoutDialect());
+        springTemplateEngine.addDialect(new SpringSecurityDialect());
+        return springTemplateEngine;
+    }
+
+    @Bean(name = "viewResolver")
+    public ViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setCharacterEncoding("UTF-8");
+        viewResolver.setOrder(1);
+        viewResolver.setCache(false);
+        return viewResolver;
+    }
 
 //    @Override
 //    public void configureDefaultServletHandling(
@@ -80,5 +118,21 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 //    public HibernateJpaSessionFactoryBean sessionFactory() {
 //        return new HibernateJpaSessionFactoryBean();
 //    }
+
+    @Bean
+    public FlowHandlerMapping flowHandlerMapping() {
+        FlowHandlerMapping handlerMapping = new FlowHandlerMapping();
+        handlerMapping.setOrder(-1);
+        handlerMapping.setFlowRegistry(this.webFlowConfig.flowRegistry());
+        return handlerMapping;
+    }
+
+    @Bean
+    public FlowHandlerAdapter flowHandlerAdapter() {
+        FlowHandlerAdapter handlerAdapter = new FlowHandlerAdapter();
+        handlerAdapter.setFlowExecutor(this.webFlowConfig.flowExecutor());
+        handlerAdapter.setSaveOutputToFlashScopeOnRedirect(true);
+        return handlerAdapter;
+    }
 
 }
---
+++ src/main/java/com/Aleksandr/Cake/WebFlowConfig.java
@@ -0,0 +1,53 @@
+package com.Aleksandr.Cake;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.webflow.config.AbstractFlowConfiguration;
+import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
+import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
+import org.springframework.webflow.executor.FlowExecutor;
+import org.springframework.webflow.mvc.builder.MvcViewFactoryCreator;
+
+import java.util.Collections;
+
+@Configuration
+public class WebFlowConfig extends AbstractFlowConfiguration {
+
+    @Autowired
+    private WebConfig webConfig;
+
+    @Bean
+    public FlowDefinitionRegistry flowRegistry() {
+        return getFlowDefinitionRegistryBuilder(flowBuilderServices())
+                .addFlowLocation("classpath:/flows/shoppingCart-flow.xml", "shoppingCartFlow")
+                .build();
+    }
+
+    @Bean
+    public FlowExecutor flowExecutor() {
+        return getFlowExecutorBuilder(flowRegistry()).build();
+    }
+
+    @Bean
+    public FlowBuilderServices flowBuilderServices() {
+        return getFlowBuilderServicesBuilder()
+                .setViewFactoryCreator(mvcViewFactoryCreator())
+                .setDevelopmentMode(true).build();
+    }
+
+    @Bean
+    public MvcViewFactoryCreator mvcViewFactoryCreator() {
+        MvcViewFactoryCreator factoryCreator = new MvcViewFactoryCreator();
+        factoryCreator.setViewResolvers(
+                Collections.singletonList(this.webConfig.viewResolver()));
+        factoryCreator.setUseSpringBeanBinding(true);
+        return factoryCreator;
+    }
+
+//    @Bean
+//    public LocalValidatorFactoryBean validator() {
+//        return new LocalValidatorFactoryBean();
+//    }
+    
+}
